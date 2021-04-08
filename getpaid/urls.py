0a46dd1a2232f59c5b8307a9c3f72c321171be94
Marcin Nowak <marcin.j.nowak@gmail.com>
2021-04-08 22:32:29 +0200

    remove ^ and $ from urls

--- getpaid/urls.py
+++ getpaid/urls.py
@@ -7,9 +7,9 @@ for backend_name, urls in list(import_backend_modules('urls').items()):
     includes_list.append(path(r'^%s/' % backend_name, include(urls)))
 
 urlpatterns = [
-    path(r'^new/payment/(?P<currency>[A-Z]{3})/$', NewPaymentView.as_view(), name='getpaid-new-payment'),
-    path(r'^payment/success/(?P<pk>\d+)/$', FallbackView.as_view(success=True), name='getpaid-success-fallback'),
-    path(r'^payment/failure/(?P<pk>\d+)$', FallbackView.as_view(success=False), name='getpaid-failure-fallback'),
+    path(r'new/payment/(?P<currency>[A-Z]{3})/', NewPaymentView.as_view(), name='getpaid-new-payment'),
+    path(r'payment/success/(?P<pk>\d+)/', FallbackView.as_view(success=True), name='getpaid-success-fallback'),
+    path(r'payment/failure/(?P<pk>\d+)/', FallbackView.as_view(success=False), name='getpaid-failure-fallback'),
 ]
 
 urlpatterns += includes_list
